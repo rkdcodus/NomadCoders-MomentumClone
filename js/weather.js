bf74ce8ce8e381aea6c89eeae2fe4f9333202f35
rkdcodus <rcd7275@naver.com>
2022-07-15 03:33:08 +0900

    위치, 날씨 구현

---
+++ js/weather.js
@@ -0,0 +1,25 @@
+const MY_KEYS = "951655ecd1df991cfbadc6a5a197ebae";
+
+function onGeoOK(position){
+    const lat = position.coords.latitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${MY_KEYS}&units=metric`;
+    fetch(url)
+        .then((response) => response.json())
+        .then((data)=> {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            weather.innerText = `${data.weather[0].main} ${data.main.temp}도`;
+            city.innerText = data.name;
+        })
+
+}
+
+function onGeoError(){
+    alert("Don't find your location and weather")
+}
+
+
+
+
+navigator.geolocation.getCurrentPosition(onGeoOK,onGeoError);
